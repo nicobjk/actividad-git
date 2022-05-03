da00519a18184be9c22abf11e07407a206cac6c5
tomasamayaa1 <tomasamayaa1@gmail.com>
2022-05-03 16:10:31 -0300

    agregue multi y division

--- archivoB.js
+++ archivoB.js
@@ -1,1 +1,8 @@
 console.log ("Soy en archivoB"); 
+function multiplicacion(numeroA, numeroB){
+    return numeroA * numeroB
+}
+
+function division(numeroA, numeroB){
+    return numeroA / numeroB
+}
